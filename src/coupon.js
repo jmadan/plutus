42a958f25364827795599d58554de436e195dc29
Jasdeep Madan <jasdeepm@gmail.com>
2019-12-20 11:28:15 +1100

    using hooks to update data displayed on the page

--- src/coupon.js
+++ src/coupon.js
@@ -1,21 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import { Typography } from '@material-ui/core'
+import { Typography, Backdrop, CircularProgress } from '@material-ui/core'
 import { useParams } from 'react-router-dom'
 const rp = require("request-promise")
+const cheerio = require("cheerio")
 
-const useStyles = makeStyles(theme => ({}));
+const useStyles = makeStyles(theme => ({
+    backdrop: {
+      zIndex: theme.zIndex.drawer + 1,
+      color: '#fff',
+    },
+  }));
 
 const fetchCoupons = async (category) => {
     let coupons = []
     let options = {
-        uri: `https://www.ozbargain.com.au/cat/${category}`,
+        uri: `https://cors-anywhere.herokuapp.com/https://www.ozbargain.com.au/cat/${category}`,
         headers: {
           'Content-Type': 'text/html',
           'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
           'Cache-Control': 'no-cache',
           'Connection': 'keep-alive',
-          'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
+          'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
+          'Origin': 'https://plutus-god-of-wealth.herokuapp.com/'
         }
       };
 
@@ -48,18 +55,37 @@ const fetchCoupons = async (category) => {
 
 export default function Coupon() {
   const classes = useStyles();
+  const [valid_coupons, setCoupons] = useState([])
+  const [working, setWorking] = useState(false)
   let { cat } = useParams();
 
-  let active_coupons = fetchCoupons(cat)
+  useEffect(() => {
+      async function getCoupons(cat){
+          setWorking(true)
+          let result = await fetchCoupons(cat)
+          setCoupons(Object.assign(valid_coupons, result))
+          console.log(JSON.stringify(valid_coupons))
+          setWorking(false)
+      }
+      getCoupons(cat)
+  }, [cat])
 
+  
+//   let active_coupons = fetchCoupons(cat)
 
   return (
     <React.Fragment>
         <Typography>
         Coupon page - {cat}
         <br />
-        {active_coupons.length}
+        {working ? <Backdrop
+            className={classes.backdrop}
+            open={working}
+         >
+            <CircularProgress color="inherit" />
+        </Backdrop> : valid_coupons.map(c => c.title)}
         </Typography>
+        
     </React.Fragment>
   );
 }
